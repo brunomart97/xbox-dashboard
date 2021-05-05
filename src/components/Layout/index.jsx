c439d7dc8fbf6b1e9ae29b2c6914e8327a5b1072
Bruno Martins <brunomart97@hotmail.com>
2021-05-05 13:07:41 -0300

    Layout and sidebar

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+import Sidebar from '../Sidebar';
+
+export default function Layout() {
+    return(
+        <div className="layout">
+            <Sidebar />
+        </div>
+    )
+};
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,32 @@
+import './styles.scss';
+
+import { SiXbox } from 'react-icons/si';
+import { RiHome8Line, RiStore2Line, RiUserVoiceLine } from 'react-icons/ri';
+import { BiJoystick, BiMessageSquareDetail } from 'react-icons/bi';
+
+export default function Sidebar() {
+    return(
+        <sidebar className="sidebar">
+            <div className="top">
+                <div className="logo">
+                    <SiXbox />
+                </div>
+
+                <div className="menu">
+                    <button className="active"><RiHome8Line /> Dashboard</button>
+                    <button><BiJoystick /> Games</button>
+                    <button><RiStore2Line /> Store</button>
+                    <button><BiMessageSquareDetail /> Messages</button>
+                    <button><RiUserVoiceLine /> Friends</button>
+                </div>
+            </div>
+
+            <div className="cardFooter">
+                <h3>Cyber Week Sale</h3>
+                <p>Up to 55% off select digital games</p>
+                <img src="/img/cardFooter.svg" alt="Cyber Week Sale"/>
+                <button>View</button>
+            </div>
+        </sidebar>
+    )
+};
